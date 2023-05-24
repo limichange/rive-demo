7f17834604de4e9b50b4cfe0b9b7d606a31a7ddf
limichange <limichange@hotmail.com>
2023-05-24 15:34:29 +0800

    feat: test new file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -49,17 +49,42 @@ function useDisplaySize(
 }
 
 export default function Simple() {
+  const [scrollDownBoolean, setScrollDownBoolean] = useState(false)
   const { rive, RiveComponent } = useRive({
-    src: '/FeVwYh6AP0iDHMKd3jZPdg.riv',
+    src: '/N_B1nC15fkCj2i6faPwsAA.riv',
     artboard: 'create-feature-01',
     autoplay: true,
     stateMachines: 'State Machine 1',
   })
 
-  // useDisplaySize()
+  const stateMachineInput = useStateMachineInput(
+    rive,
+    'State Machine 1',
+    'scroll-down',
+    false
+  )
+
+  console.log('stateMachineInput', stateMachineInput, scrollDownBoolean)
 
   return (
     <div style={{ height: '90vh' }}>
+      <div>
+        <label style={{ color: 'white' }}>
+          <input
+            type='checkbox'
+            checked={scrollDownBoolean}
+            onChange={(e) => {
+              if (stateMachineInput) {
+                setScrollDownBoolean(e.target.checked)
+
+                stateMachineInput.value = e.target.checked
+              }
+            }}
+          />
+          Scroll Down
+        </label>
+      </div>
+
       <RiveComponent />
     </div>
   )
